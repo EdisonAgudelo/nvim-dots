1c08986adef8b0bf125fafb48c7ea9cbe6f405f3
Edison Agudelo <edison.1998@hotmail.es>
2025-04-09 08:48:00 -0500

    Add folding tool

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -125,7 +125,6 @@ require("lazy").setup({
             keys = {
                 {"gD", "<cmd>lua vim.lsp.buf.declaration()<CR>",  desc = "Goto Declaration"},
                 {"gd", "<cmd>lua vim.lsp.buf.definition()<CR>",  desc = "Goto Definition"},
-                {"gr", "<cmd>lua vim.lsp.buf.references()<CR>",  desc = "Show References"},
             },
         },
         {
@@ -137,11 +136,19 @@ require("lazy").setup({
         },
         {
             "williamboman/mason-lspconfig.nvim",
-            dependecies = {"nvim-lspconfig", "saghen/blink.cmp"},
+            dependencies = {"nvim-lspconfig", "saghen/blink.cmp"},
             config = function()
 
                 local capabilities = vim.lsp.protocol.make_client_capabilities()
+
+                -- for text auto completion
                 capabilities = vim.tbl_deep_extend('force', capabilities, require('blink.cmp').get_lsp_capabilities({}, false))
+
+                -- for folding capabilities
+                capabilities.textDocument.foldingRange = {
+                    dynamicRegistration = false,
+                    lineFoldingOnly = true
+                }
 
                 local custom_settings = {
                     ["pylsp"] = {
@@ -151,6 +158,12 @@ require("lazy").setup({
                                 plugins = {
                                     pycodestyle = {
                                         enabled = false,
+                                    },
+                                    rope_autoimport = {
+                                        enabled = true,
+                                        completions = {
+                                            enabled = false
+                                        }
                                     }
                                 }
                             }
@@ -440,6 +453,63 @@ require("lazy").setup({
                 window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
             }
         },
+        {
+            "aznhe21/actions-preview.nvim",
+            config = function()
+                vim.keymap.set({ "v", "n" }, "gf", require("actions-preview").code_actions)
+            end,
+        },
+        {
+            "kevinhwang91/nvim-ufo",
+            dependencies = "kevinhwang91/promise-async",
+            config = function ()
+                vim.o.foldcolumn = '1' -- '0' is not bad
+                vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+                vim.o.foldlevelstart = 99
+                vim.o.foldenable = true
+
+                -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
+                vim.keymap.set('n', 'zR', require('ufo').openAllFolds,  { desc = "Open all folds"})
+                vim.keymap.set('n', 'zM', require('ufo').closeAllFolds, { desc = "Close all folds"})
+
+
+                local fold_formater = function(virtText, lnum, endLnum, width, truncate)
+                    local newVirtText = {}
+                    local suffix = (' 󰁂 %d '):format(endLnum - lnum)
+                    local sufWidth = vim.fn.strdisplaywidth(suffix)
+                    local targetWidth = width - sufWidth
+                    local curWidth = 0
+                    for _, chunk in ipairs(virtText) do
+                        local chunkText = chunk[1]
+                        local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+                        if targetWidth > curWidth + chunkWidth then
+                            table.insert(newVirtText, chunk)
+                        else
+                            chunkText = truncate(chunkText, targetWidth - curWidth)
+                            local hlGroup = chunk[2]
+                            table.insert(newVirtText, {chunkText, hlGroup})
+                            chunkWidth = vim.fn.strdisplaywidth(chunkText)
+                            -- str width returned from truncate() may less than 2nd argument, need padding
+                            if curWidth + chunkWidth < targetWidth then
+                                suffix = suffix .. (' '):rep(targetWidth - curWidth - chunkWidth)
+                            end
+                            break
+                        end
+                        curWidth = curWidth + chunkWidth
+                    end
+                    table.insert(newVirtText, {suffix, 'MoreMsg'})
+                    return newVirtText
+                end
+
+
+                require("ufo").setup({
+                    fold_virt_text_handler = fold_formater,
+                    provider_selector = function (bufnr, filetype, buftype)
+                        return { "lsp", "indent" }
+                    end
+                })
+            end
+        }
     },
     -- automatically check for plugin updates
     checker = { enabled = false },
