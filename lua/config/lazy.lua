f5679fd71357fc087496b8eb4ad5a90f2d9fc9ef
Edison Agudelo <edison.1998@hotmail.es>
2025-05-02 01:02:06 -0500

    chore: add .venv python venvs and new copy paste style

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -430,8 +430,8 @@ require("lazy").setup({
             dependencies = { 'neovim/nvim-lspconfig', 'nvim-telescope/telescope.nvim'},
             opts = {
                 -- Your options go here
-                -- name = "venv",
-                -- auto_refresh = false
+                name = {"venv", ".venv"},
+                auto_refresh = false
             },
             event = 'VeryLazy', -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
 
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -11,7 +11,7 @@ vim.opt.expandtab = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 
-vim.opt.clipboard = "unnamedplus"
+-- vim.opt.clipboard = "unnamedplus"
 
 vim.opt.virtualedit = "block"
 vim.opt.inccommand = "split"
@@ -28,3 +28,6 @@ vim.keymap.set("n", "<leader><Tab>", "<cmd>tabn<CR>", {desc = "Next Tab"})
 vim.keymap.set("n", "<s-leader><Tab>", "<cmd>tabp<CR>", {desc = "Prev Tab"})
 vim.keymap.set("n", "<leader>n", "<cmd>bn<CR>", {desc = "Change to nex buffer"})
 vim.keymap.set("n", "<leader>p", "<cmd>bp<CR>", {desc = "Change to prev buffer"})
+
+vim.keymap.set("v", "<leader>c", '"+y', {desc = "Copy to clipboard"})
+vim.keymap.set("n", "<leader>c", '"+p', {desc = "Paste from clipboard"})
